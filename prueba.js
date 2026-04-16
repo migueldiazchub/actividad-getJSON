42a68b30101e4b8334819feb39c2b139604f9dd6
migle <mikigalletas@gmail.com>
2026-04-16 22:21:10 +0200

    fin pruebas1

--- prueba.js
+++ prueba.js
@@ -7,22 +7,55 @@ $(document).ready(function () {
     return datos;
   }
 
-  /*
-  "success": true,
-  "deck_id": "lyujoht3n7b6",
-  "remaining": 52,
-  "shuffled": true
-  */
+  //objeto que contendrá el JSON de la baraja. Formato:
+  // {
+  //  "success": true,
+  //  "deck_id": "infdwvf2bo8h",
+  //  "remaining": 52,
+  //  "shuffled": false
+  //  }
+  let barajaJSON = {};
 
-  //parte trasera carta
-  //"https://deckofcardsapi.com/static/img/back.png"
+  //array que contendrá las cartas barajadas. Formato de carta:
+  // {
+  //   "code": "9D",
+  //   "image": "https://deckofcardsapi.com/static/img/9D.png",
+  //   "images": {
+  //     "svg": "https://deckofcardsapi.com/static/img/9D.svg",
+  //     "png": "https://deckofcardsapi.com/static/img/9D.png"
+  //   },
+  //   "value": "9",
+  //   "suit": "DIAMONDS"
+  // }
+  let baraja = [];
+
+  //posición en el array de la primera carta sin sacar de la baraja
+  let posicionBaraja = 51;
+
+  /**
+   * recibe el id del elemento html y devuelve el objeto de carta correspondiente
+   * @param {string} id
+   * @returns {Object}
+   */
+  function getCarta(id) {
+    for (let carta of baraja) {
+      if (id == carta.code) {
+        return carta;
+      }
+    }
+  }
 
   $("#btn-crear").click(async function () {
-    let cartas = await getJSON(
-      "https://deckofcardsapi.com/api/deck/new/draw/?count=52",
+    let barajaJSON = await getJSON(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/",
     );
+
+    let urlCartas = "https://deckofcardsapi.com/api/deck/new/draw/?count=52";
+    urlCartas.replace("new", barajaJSON.deck_id);
+    let cartas = await getJSON(urlCartas);
     baraja = cartas.cards;
     let zindex = 200;
+
     for (let carta of baraja) {
       carta.view = false;
       carta.zindex = zindex++;
@@ -43,10 +76,6 @@ $(document).ready(function () {
       $(this).css("background-color", "green");
     }
   });
-
-  //función para sacar la primera carta de la baraja y contador
-  //para sacar las siguientes
-  let posicionBaraja = 51;
 
   function sacarCarta() {
     let carta = baraja[posicionBaraja--];
@@ -131,14 +160,4 @@ $(document).ready(function () {
       80,
     );
   });
-
-  let baraja = [];
-
-  function getCarta(id) {
-    for (let carta of baraja) {
-      if (id == carta.code) {
-        return carta;
-      }
-    }
-  }
 });
--- script.js
+++ script.js
@@ -1,1 +1,88 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  async function getJSON(url) {
+    let datos = {};
+    await $.getJSON(url, function (data) {
+      datos = data;
+    });
+    return datos;
+  }
+
+  //objeto que contendrá el JSON de la baraja. Formato:
+  // {
+  //  "success": true,
+  //  "deck_id": "infdwvf2bo8h",
+  //  "remaining": 52,
+  //  "shuffled": false
+  //  }
+  let barajaJSON = {};
+
+  //array que contendrá las cartas barajadas. Formato de carta:
+  // {
+  //   "code": "9D",
+  //   "image": "https://deckofcardsapi.com/static/img/9D.png",
+  //   "images": {
+  //     "svg": "https://deckofcardsapi.com/static/img/9D.svg",
+  //     "png": "https://deckofcardsapi.com/static/img/9D.png"
+  //   },
+  //   "value": "9",
+  //   "suit": "DIAMONDS"
+  // }
+  let baraja = [];
+
+  //posición en el array de la primera carta sin sacar de la baraja
+  let posicionBaraja = 51;
+
+  /**
+   * recibe el id del elemento html y devuelve el objeto de carta correspondiente
+   * @param {string} id
+   * @returns {Object}
+   */
+  function getCarta(id) {
+    for (let carta of baraja) {
+      if (id == carta.code) {
+        return carta;
+      }
+    }
+  }
+
+  $("#btn-abrir").click(function () {
+    $("#zona-juego").slideDown(1200);
+  });
+
+  //evento para crear una baraja aleatorizada y colocar todas sus cartas en la zona de baraja
+  $("#btn-crear-baraja").click(async function () {
+    let barajaJSON = await getJSON(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/",
+    );
+
+    let urlCartas =
+      "https://deckofcardsapi.com/api/deck/placeholder/draw/?count=52";
+    urlCartas.replace("placeholder", barajaJSON.deck_id);
+    let cartas = await getJSON(urlCartas);
+    baraja = cartas.cards;
+    let zindex = 200;
+
+    for (let carta of baraja) {
+      carta.view = false;
+      carta.zindex = zindex++;
+
+      let marcoCarta = $("<div>")
+        .addClass("carta-baraja")
+        .attr("id", carta.code);
+
+      marcoCarta.append(
+        $("<img>")
+          .addClass("imagen-carta")
+          .attr("src", "https://deckofcardsapi.com/static/img/back.png"),
+      );
+
+      $("#baraja").append(marcoCarta);
+    }
+
+    //$("#btn-sacar-carta").show();
+
+    if (baraja[0] != undefined) {
+      $(this).css("background-color", "green");
+    }
+  });
+});
